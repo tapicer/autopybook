9f12425a60e446eb73bb9f9d959a163cdf4023b1
Jonathan Tapicer <tapicer@gmail.com>
2012-03-26 20:04:37 -0300

    Added support for multiple notes identified by some id.

--- run.py
+++ run.py
@@ -1,22 +1,46 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class ServerHandler(BaseHTTPRequestHandler):
+    def get_id(self):
+        # filter out the path used for the id
+        return filter(lambda c: c.isalnum(), self.path)
+    
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        initial_text = self.read_file('data')
-        content = self.read_file('index.html')
-        content = content.replace('{{initial_text}}', self.html_escape(initial_text))
-        self.wfile.write(content)
+        if self.path == '/favicon.ico':
+            self.send_response(404)
+            self.end_headers()
+        elif self.path == '/jquery.filter_input.js':
+            self.send_response(200)
+            self.send_header("Content-type", "text/javascript")
+            self.end_headers()
+            self.wfile.write(self.read_file('jquery.filter_input.js'))
+        else:
+            self.send_response(200)
+            self.end_headers()
+            id = self.get_id()
+            if id:
+                initial_text = self.read_file(id)
+            else:
+                initial_text = 'To start using autopybook write some id and press the "Load" button (or hit enter).'
+            content = self.read_file('index.html')
+            content = content.replace('{{initial_text}}', self.html_escape(initial_text))
+            content = content.replace('{{initial_id}}', id)
+            content = content.replace('{{initial_expl}}', 'Write some text below and it will be auto-saved for your id.' if id else '')
+            self.wfile.write(content)
     
     def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-        data = self.rfile.read(int(self.headers.getheader('content-length')))
-        self.write_file('data', data)
+        id = self.get_id()
+        if id:
+            self.send_response(200)
+            self.end_headers()
+            data = self.rfile.read(int(self.headers.getheader('content-length')))
+            self.write_file(id, data)
     
     def read_file(self, path):
-        return open(path, 'r').read()
+        try:
+            return open(path, 'r').read()
+        except IOError:
+            return ''
     
     def write_file(self, path, content):
         open(path, 'w').write(content)
